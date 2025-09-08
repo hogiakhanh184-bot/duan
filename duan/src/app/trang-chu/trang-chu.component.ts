e833d9334cb61dacbe3d744235327d703e750dcb
khanhv-bip <khanhhgps40891@gmail.com>
2025-09-08 22:05:57 +0700

    h

--- duan/src/app/trang-chu/trang-chu.component.ts
+++ duan/src/app/trang-chu/trang-chu.component.ts
@@ -9,22 +9,5 @@ import { CommonModule, CurrencyPipe } from '@angular/common';
   styleUrls: ['./trang-chu.component.css']
 })
 export class TrangChuComponent {
-  title = 'Trang chủ';
-  products = [
-    {
-      name: 'Áo thun nam',
-      price: 199000,
-      image: 'https://via.placeholder.com/150?text=Ao+Thun'
-    },
-    {
-      name: 'Giày sneaker',
-      price: 499000,
-      image: 'https://via.placeholder.com/150?text=Giay+Sneaker'
-    },
-    {
-      name: 'Túi xách nữ',
-      price: 299000,
-      image: 'https://via.placeholder.com/150?text=Tui+Xach'
-    }
-  ];
+ 
 }
